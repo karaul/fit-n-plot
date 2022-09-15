552da44ad5d2ccf0717ecb095bee0e804f512655
Evgeny Votyakov <e.votyakov@cyi.ac.cy>
2022-09-15 18:46:34 +0300

    The very first commit

--- index.md
+++ index.md
@@ -12,33 +12,34 @@
 		box-sizing: border-box;
 	  }
 
-	.container { border: 5p;  padding: 6px;}
-	.column1 { width: 50%; float: left; padding: 2px; }	
-	.column2 { width: 50%; height: 240px; float: left; padding: 2px;}  
-	
-		</style>			
-	</head>
+    .container { border: 5p;  padding: 6px;}
+    .column1 { width: 50%; float: left; padding: 2px; }
+    .column2 { width: 50%; height: 240px; float: left; padding: 2px;}
+
+    	</style>
+    </head>
     <body>
 
-		<p> 
-			<label for="myfile">Input Data File:</label>
-			<input id="myfile" type="file" value='' style="padding: 20px;" /> 
-			<!-- <input id="adddata" type="checkbox" value="false"> <label for="adddata">add data from file</label>  -->
-        	<!-- <p> <input id="addfile" type="file" value=''/> </p> -->        
-			<!-- <label for="xaxis">Choose x-axis:</label> -->
-			<!-- <select name="x" id="xaxis"> -->
-				<!-- <option value="distance">distance</option>  -->
-				<!-- <option value="timestamp">time</option> -->
-			<!-- </select>  -->
+    	<p>
+    		<label for="myfile">Input Data File:</label>
+    		<input id="myfile" type="file" value='' style="padding: 20px;" />
+    		<!-- <input id="adddata" type="checkbox" value="false"> <label for="adddata">add data from file</label>  -->
+        	<!-- <p> <input id="addfile" type="file" value=''/> </p> -->
+    		<!-- <label for="xaxis">Choose x-axis:</label> -->
+    		<!-- <select name="x" id="xaxis"> -->
+    			<!-- <option value="distance">distance</option>  -->
+    			<!-- <option value="timestamp">time</option> -->
+    		<!-- </select>  -->
 
 
-		</p>
+    	</p>
 
-		Fit Function: 
-<textarea cols="70" rows="1" id="eq"> 5.88 - 5.063*x + 1.03*x*x  </textarea>
-         &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp  &nbsp 
-		<label for="filehist">Histogram File:</label>
-		<input id="filehist" type="file" value='' style="padding: 20px;" /> 
+    	Fit Function:
+
+<textarea cols="70" rows="1" id="eq"> 5.88 - 5.063*x + 1.03*x\*x </textarea>
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+<label for="filehist">Histogram File:</label>
+<input id="filehist" type="file" value='' style="padding: 20px;" />
 
 <br>
 
@@ -59,26 +60,21 @@ to <input type="text" width="5" id="x2" value="10"></p> -->
 <!-- <font size=-1>&nbsp  Move mouse to see values. Click and drag to zoom. Double-click to restore</font><br/> -->
 
 <!-- <div id="plotarea" style="width:640px; height:480px;"></div> -->
-		
-		
-
 
         <!-- <div id="plothist" style="width:600px; height:300px;"></div> -->
 
 <div class="container">
 
-		<div class="column1" id="plotarea" style="width:50%; height:480px; padding-left: 0pt; margin-bottom: 10px;"></div>
+    	<div class="column1" id="plotarea" style="width:50%; height:480px; padding-left: 0pt; margin-bottom: 10px;"></div>
 
-		<div class="column2" id="plothist" style="width:50%; height:480px; padding-right: 0pt; margin-bottom: 10px;"></div>
-	</div>
+    	<div class="column2" id="plothist" style="width:50%; height:480px; padding-right: 0pt; margin-bottom: 10px;"></div>
+    </div>
 
-		<input id="cleanall" type="button" value='Remove all fits'/> 
-		<input id="cleanlast" type="button" value='Remove last fit' style="margin-left: 4em"/> 
-
-
+    	<input id="cleanall" type="button" value='Remove all fits'/>
+    	<input id="cleanlast" type="button" value='Remove last fit' style="margin-left: 4em"/>
 
 </body>
 
-		<script type="text/javascript" src="./src/index.js"></script> 
+    	<script type="text/javascript" src="./src/index.js"></script>
 
 </html>
--- src/index.js
+++ src/index.js
@@ -1,193 +1,213 @@
-document.addEventListener('DOMContentLoaded', function () {
-	
-	
-	function guessDelimiters (text, possibleDelimiters) {
+document.addEventListener("DOMContentLoaded", function () {
+  function guessDelimiters(text, possibleDelimiters) {
     return possibleDelimiters.filter(weedOut);
 
-    function weedOut (delimiter) {
-        var cache = -1;
-        return text.split('\n').every(checkLength);
+    function weedOut(delimiter) {
+      var cache = -1;
+      return text.split("\n").every(checkLength);
 
-        function checkLength (line) {
-            if (!line) {
-                return true;
-            }
-
-            var length = line.split(delimiter).length;
-            if (cache < 0) {
-                cache = length;
-            }
-            return cache === length && length > 1;
+      function checkLength(line) {
+        if (!line) {
+          return true;
         }
+
+        var length = line.split(delimiter).length;
+        if (cache < 0) {
+          cache = length;
+        }
+        return cache === length && length > 1;
+      }
     }
-}
+  }
 
-function transpose(a) {
-	return a[0].map(function (_, c) {
-		return a.map(function (r) {
-			return r[c];
-		});
-	});
-}
+  function transpose(a) {
+    return a[0].map(function (_, c) {
+      return a.map(function (r) {
+        return r[c];
+      });
+    });
+  }
 
-document.getElementById('cleanall').onclick = function (e) {
-     for (let k = chart.options.data.length - 1; k > 1; k--) {
-			chart.options.data.pop()
-		}
-	chart.render();	
-}
-document.getElementById('cleanlast').onclick = function (e) {
-	chart.options.data.pop()
-	chart.render();	
-}
+  document.getElementById("cleanall").onclick = function (e) {
+    for (let k = chart.options.data.length - 1; k > 1; k--) {
+      chart.options.data.pop();
+    }
+    chart.render();
+  };
+  document.getElementById("cleanlast").onclick = function (e) {
+    chart.options.data.pop();
+    chart.render();
+  };
 
-var fReader = new FileReader();
-var fReaderHist = new FileReader();
+  var fReader = new FileReader();
+  var fReaderHist = new FileReader();
 
-var fileInput = document.getElementById('myfile');
-var fileHist = document.getElementById('filehist');
+  var fileInput = document.getElementById("myfile");
+  var fileHist = document.getElementById("filehist");
 
+  var filename = "";
 
-var filename = "";
+  fileInput.onchange = function (e) {
+    var file = this.files[0];
+    filename = file.name;
+    fReader.readAsText(file);
+  };
 
-fileInput.onchange = function (e) {
-	var file = this.files[0];
-	filename = file.name;
-	fReader.readAsText(file);
-}
+  fileHist.onchange = function (e) {
+    var file = this.files[0];
+    filename = file.name;
+    fReaderHist.readAsText(file);
+  };
 
-fileHist.onchange = function (e) {
-	var file = this.files[0];
-	filename = file.name;
-	fReaderHist.readAsText(file);
-}
+  var xStart, xEnd;
+  var dataY = [];
+  var dataYErr = [];
 
-var	xStart, xEnd;
-var dataY = [];
-var dataYErr = [];
+  var chart = new CanvasJS.Chart("plotarea", {
+    title: { text: "Data Plot + Fit Function" },
+    zoomEnabled: true,
+    zoomType: "xy", // "x","y", "xy"
+    //,rangeChanging: updateMapSegment
+    //,animationEnabled: true
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    //axisX: { gridThickness: 0.5, snapToDataPoint: true },
+    //axisY: { gridThickness: 0.5, snapToDataPoint: true },
+    //axisY2: axisY2ops,
+    legend: {
+      fontSize: 15,
+      horizontalAlign: "right", // "center" , "right" "left"
+      verticalAlign: "bottom", // "top" , "bottom" center
+      //,cursor: "pointer"
+      //,itemclick: toggleDataSeries
+    },
+    data: [
+      {
+        type: "error",
+        showInLegend: false,
+        //legendText: "input",
+        name: "Error",
+        //whiskerLength: 0.5,
+        toolTipContent:
+          '<span style="color:#C0504E">{name}</span>: {y[0]} - {y[1]}',
+        dataPoints: dataYErr,
+      },
+      {
+        type: "scatter",
+        showInLegend: false,
+        //legendText: "Input Data",
+        markerType: "circle", //"triangle",
+        markerColor: "#ffffff",
+        //fillOpacity: .1,
+        //markerSize: 4,
+        markerBorderColor: "#00B000",
+        markerBorderThickness: 0.5,
+        toolTipContent: "<b>X: </b>{x} <br/><b>Yexp: </b>{y}k",
+        dataPoints: dataY,
+      },
+    ],
+  });
 
+  fReader.onload = function (e) {
+    //console.log(e.target.result); /// <-- this contains an ArrayBuffer
+    var text = e.target.result;
+    var lines = text.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
 
-var chart = new CanvasJS.Chart("plotarea", {
-	title:{ text: "Data Plot + Fit Function"},	
-	    zoomEnabled: true,
-		zoomType: "xy",  // "x","y", "xy"
-		//,rangeChanging: updateMapSegment
-		//,animationEnabled: true
-		theme: "light2", // "light1", "light2", "dark1", "dark2"		
-		//axisX: { gridThickness: 0.5, snapToDataPoint: true },
-		//axisY: { gridThickness: 0.5, snapToDataPoint: true },
-		//axisY2: axisY2ops,
-		legend: {
-			fontSize: 15,
-            horizontalAlign: "right", // "center" , "right" "left"
-            verticalAlign: "bottom",  // "top" , "bottom" center
-			//,cursor: "pointer"
-			//,itemclick: toggleDataSeries
-		},
-		data: [
-		   { type: "error",
-		   showInLegend: false,
-           //legendText: "input",
-		   name: "Error",
-		   //whiskerLength: 0.5,
-		   toolTipContent: "<span style=\"color:#C0504E\">{name}</span>: {y[0]} - {y[1]}",
-		   dataPoints: dataYErr},
-		   { type: "scatter",
-		   showInLegend: false,
-           //legendText: "Input Data",
-		   markerType: "circle", //"triangle",
-		   markerColor: "#ffffff",
-		   //fillOpacity: .1,
-		   //markerSize: 4,
-		   markerBorderColor: "#00B000",
-		   markerBorderThickness: 0.5,
-		   toolTipContent: "<b>X: </b>{x} <br/><b>Yexp: </b>{y}k",
-		   dataPoints: dataY}		  
-		   ]
-		});	
-
-
-fReader.onload = function (e) {
-	//console.log(e.target.result); /// <-- this contains an ArrayBuffer
-	var text = e.target.result;
-	var lines = text.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
-
-	linestart = 0;
-	for (var i = linestart; i < lines.length; i++) {
-		lines[i] = lines[i].trim().replace(/\s{2,}/g, ' ');
-	}
-	var delim = guessDelimiters(lines[8]+'\n'+lines[9], [' ','\t',','] );
-	xStart = 999999999;
-	xEnd = -999999999;
-	for (var i = linestart; i < lines.length; i++) {
-		var s = lines[i].split(delim[0]);
-		if (isNaN(parseFloat(s[0]))) {} else {
-		  var x=parseFloat(s[0]);
-  		  dataY.push( {x: x, y: parseFloat(s[1])} );
-	 	  dataYErr.push( {x: x, y: [ parseFloat(s[1])-parseFloat(s[2]), parseFloat(s[1])+parseFloat(s[2]) ] });
-		  if( xStart > x  ) { xStart = x;}
-		  if( xEnd < x  ) { xEnd = x;}
-		}
-	}
-	
-	//console.log(dataY);
-	//console.log(xEnd);
-	chart.render();
-	//data = transpose(data);
-};
-
-fReaderHist.onload = function (e) {
-	//console.log(e.target.result); /// <-- this contains an ArrayBuffer
-	var text = e.target.result;
-	var lines = text.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
-
-	linestart = 0;
-	for (var i = linestart; i < lines.length; i++) {
-		lines[i] = lines[i].trim().replace(/\s{2,}/g, ' ');
-	}
-	var delim = guessDelimiters(lines[8]+'\n'+lines[9], [' ','\t',','] );
-	//xStart = 999999999;
-	//xEnd = -999999999;
-	
-    var chart = new CanvasJS.Chart("plothist", {
-	   theme: "light2", 
-	   title:{ text: "Parameter PDF"},	data: [{ type: "column", dataPoints: []},]
-	});	
-	
-	for (var i = linestart; i < lines.length; i++) {
-		var s = lines[i].split(delim[0]);
-		if (isNaN(parseFloat(s[0]))) {} else {
-			chart.options.data[0].dataPoints.push( { x: parseFloat(s[0]), y: parseFloat(s[1]) } );
-		  //if( xStart > x  ) { xStart = x;}
-		  //if( xEnd < x  ) { xEnd = x;}
-		}
-	}
-	
-	chart.render();
-	//data = transpose(data);
-};
-
-document.getElementById('plot').onclick =  function () {
-        var eq = document.getElementById("eq").value;
-        eval("fn = function(x) { return " + eq + " ;} ");
-        var data = [];
-		var Nbin=100;
-		var xs = (xEnd-xStart)/Nbin;
-		// console.log(xStart,xEnd,xs)
-        for (var i = 0; i < Nbin+1; i++) {
-          var x = xStart + i * xs;
-		  //   return [ 5.88 - 5.063*x + 1.03*x*x ];
-          var y = fn(x);
-		  //var y = 5.88 - 5.063*x + 1.03*x*x;
-		  console.log(x,y);
-          data.push({x: x, y: y});
+    linestart = 0;
+    for (var i = linestart; i < lines.length; i++) {
+      lines[i] = lines[i].trim().replace(/\s{2,}/g, " ");
+    }
+    var delim = guessDelimiters(lines[8] + "\n" + lines[9], [" ", "\t", ","]);
+    xStart = 999999999;
+    xEnd = -999999999;
+    for (var i = linestart; i < lines.length; i++) {
+      var s = lines[i].split(delim[0]);
+      if (isNaN(parseFloat(s[0]))) {
+      } else {
+        var x = parseFloat(s[0]);
+        dataY.push({ x: x, y: parseFloat(s[1]) });
+        dataYErr.push({
+          x: x,
+          y: [
+            parseFloat(s[1]) - parseFloat(s[2]),
+            parseFloat(s[1]) + parseFloat(s[2]),
+          ],
+        });
+        if (xStart > x) {
+          xStart = x;
         }
-		  chart.options.data.push( { type: "line", showInLegend: true, legendText: "fit: "+eq, dataPoints: data });
-          chart.render();
+        if (xEnd < x) {
+          xEnd = x;
+        }
+      }
+    }
 
-}
-	
-/*       var plot;  // defined below
+    //console.log(dataY);
+    //console.log(xEnd);
+    chart.render();
+    //data = transpose(data);
+  };
+
+  fReaderHist.onload = function (e) {
+    //console.log(e.target.result); /// <-- this contains an ArrayBuffer
+    var text = e.target.result;
+    var lines = text.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
+
+    linestart = 0;
+    for (var i = linestart; i < lines.length; i++) {
+      lines[i] = lines[i].trim().replace(/\s{2,}/g, " ");
+    }
+    var delim = guessDelimiters(lines[8] + "\n" + lines[9], [" ", "\t", ","]);
+    //xStart = 999999999;
+    //xEnd = -999999999;
+
+    var chart = new CanvasJS.Chart("plothist", {
+      theme: "light2",
+      title: { text: "Parameter PDF" },
+      data: [{ type: "column", dataPoints: [] }],
+    });
+
+    for (var i = linestart; i < lines.length; i++) {
+      var s = lines[i].split(delim[0]);
+      if (isNaN(parseFloat(s[0]))) {
+      } else {
+        chart.options.data[0].dataPoints.push({
+          x: parseFloat(s[0]),
+          y: parseFloat(s[1]),
+        });
+        //if( xStart > x  ) { xStart = x;}
+        //if( xEnd < x  ) { xEnd = x;}
+      }
+    }
+
+    chart.render();
+    //data = transpose(data);
+  };
+
+  document.getElementById("plot").onclick = function () {
+    var eq = document.getElementById("eq").value;
+    eval("fn = function(x) { return " + eq + " ;} ");
+    var data = [];
+    var Nbin = 100;
+    var xs = (xEnd - xStart) / Nbin;
+    // console.log(xStart,xEnd,xs)
+    for (var i = 0; i < Nbin + 1; i++) {
+      var x = xStart + i * xs;
+      //   return [ 5.88 - 5.063*x + 1.03*x*x ];
+      var y = fn(x);
+      //var y = 5.88 - 5.063*x + 1.03*x*x;
+      console.log(x, y);
+      data.push({ x: x, y: y });
+    }
+    chart.options.data.push({
+      type: "line",
+      showInLegend: true,
+      legendText: "fit: " + eq,
+      dataPoints: data,
+    });
+    chart.render();
+  };
+
+  /*       var plot;  // defined below
       var select = document.getElementById("presets");
       var presets = {
         'id': [ -10, 10, 'function(x) {\n  return x;\n}' ],
@@ -236,8 +256,5 @@ document.getElementById('plot').onclick =  function () {
       };
       plotButton.onclick = plot;
       plot();
-	*/  
-	  
-    }
-	
-);
+	*/
+});
